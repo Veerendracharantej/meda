e2fa329bcfc2f5ae15fffba98dfe4398248cef98
Veerendra Charan Tej <74517096+Veerendracharantej@users.noreply.github.com>
2022-05-07 22:19:55 +0530

    Add files via upload

---
+++ src/main/java/sdpassignment/charanTest.java
@@ -0,0 +1,28 @@
+package sdpassignment;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class charanTest {
+	@Test
+	public void addTest() {
+		assertEquals(Calc.add(2, 3),5);
+	}
+	
+	@Test
+	public void subTest() {
+		assertEquals(Calc.sub(10, 4),6);
+	}
+	
+	@Test
+	public void mulTest() {
+		assertEquals(Calc.mul(2, 3),6);
+	}
+	
+	@Test
+	public void divTest() {
+		assertEquals(Calc.div(10, 2),5);
+	}
+
+
+
